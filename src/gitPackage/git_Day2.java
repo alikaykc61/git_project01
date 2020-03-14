9246a93c6b3857ea82854e4704ef361a9a4a8bb6
ali kayikcioglu <alikaykc61@gmail.com>
2020-03-14 13:15:26 -0700

    7 trillion

--- src/gitPackage/git_Day2.java
+++ src/gitPackage/git_Day2.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 	
 	System.out.println("naaber Mudur;)) ");
 	System.out.println("check if your local is ahead");
-	
+	System.out.println("commit 7 trillion");
 	
 	
 	
